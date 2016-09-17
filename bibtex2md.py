1bbcd3fdfa7583d8eb8616ba7b1b33a5b1ce0cfd
Kenji Hisazumi <kenji@hisazumi.jp>
2016-09-17 14:01:04 +0900

    support books

--- bibtex2md.py
+++ bibtex2md.py
@@ -11,6 +11,7 @@ with open('/Users/nel/Documents/tex/nel.bib') as f:
 def get_type_of(type):
 	return [e for e in bibdb.entries if e['ENTRYTYPE'] == type]
 
+books    = get_type_of('book')
 journals = get_type_of('article')
 procs    = get_type_of('inproceedings')
 
@@ -23,8 +24,19 @@ print("permalink: pub")
 print("---")
 print("")
 
+# books
+print("# Books")
+print("")
+for e in books:
+	#print(e)
+	if "jauthor" in e:
+		print('1. ', e['author'],'(著)', e['jauthor'], '(訳)', ':', e['title'], ',', e['publisher'], ',', e['year'], ".")
+	else:
+		print('1. ', e['author'],':', e['title'], ',', e['publisher'], ',', e['year'], ".")
+	print('')
+
 # journals
-print("# Journal")
+print("# Journals")
 print("")
 for e in journals:
 	#print(e)
