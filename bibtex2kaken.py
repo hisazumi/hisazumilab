1b579bb7ed52653c150324d68292610db4e33b61
Kenji Hisazumi <kenji@hisazumi.jp>
2017-05-28 17:17:49 +0900

    Refined bibtex2kaken accoding to new style

--- bibtex2kaken.py
+++ bibtex2kaken.py
@@ -1,96 +1,27 @@
-# an exporter for hexo
+# an exporter for kaken tex
 
-import datetime
-import bibtexparser
-import unicodedata
-import re
-
-# some utilities
-
-def is_japanese(string):
-    for ch in string:
-        name = unicodedata.name(ch) 
-        if "CJK UNIFIED" in name \
-        or "HIRAGANA" in name \
-        or "KATAKANA" in name:
-            return True
-    return False
-
-def remove_commna_in_japanese(string):
-	if is_japanese(string):
-		return string.replace(',', '')
-	else:
-		return string
-
-def strip_space(string):
-	return re.sub(r' +', ' ', string)
-
-def authors(string):
-	return ",".join([apply_underline(abbrev_in_english(item)) for item in string.split('and')])
-
-def abbrev_in_english(string):
-	comma_n = string.find(',')
-	if comma_n < 0:
-		return string
-	else:
-		return string[comma_n+1:] + " " + string[0:comma_n]
-
-def apply_underline(author):
-	if 'Hisazumi' in author:
-		return '\\underline{\\underline{'+author+'}}'
-	else:
-		return author
-
-def strip(str):
-	return strip_space(str).replace(' ,', ',').replace(',', ',').replace(' .', '.')
-
-def author(str):
-	return authors(remove_commna_in_japanese(str))
-
-def emit_author(e):
-	if "jauthor" in e:
-		return e['author'] + '(著), ' + author(e['jauthor']) + '(訳)' + ': '
-	else:
-		return author(e['author']) + ': '
-
-def emit_pages(e):
-	if "pages" in e:
-		return ", pp." + e['pages']
-	else:
-		return ""	
+from bibtex import read_bib, get_type_of_bib
+from jinja2 import Environment, FileSystemLoader
 
 # read bibtex
+bib = read_bib('/Users/nel/GD/tex/nel.bib')
 
-with open('/Users/nel/Documents/tex/nel.bib') as f:
-    bibdb = bibtexparser.loads(f.read())
+# mark myself
+mes = ['Kenji Hisazumi', '久住憲嗣']
 
-def get_type_of(type):
-	return [e for e in bibdb.entries if e['ENTRYTYPE'] == type]
 
-books    = get_type_of('book')
-journals = get_type_of('article')
-procs    = get_type_of('inproceedings')
+def mark_myself(ls):
+    for e in ls:
+        for me in mes:
+            e['author'] = e['author'].replace(
+                me, '\\underline{\\underline{' + me + '}}')
+    return ls
+
 
 # emit markdown
-
-# journals
-def emit_journal(e):
-	return '\\KLbibitem ' + strip(emit_author(e) + e['title'] + ',' + e['journal'] + ',' + e['publisher'] + emit_pages(e) + ',' + e['year'] + ".")
-
-def emit_proc(e):
-	return '\\KLbibitem ' + strip(emit_author(e) + e['title'] + ',' + e['booktitle'] + emit_pages(e) + ',' + e['year'] + ".")
-
-years=[2016, 2015, 2014, 2013, 2012]
-
-for y in years:
-	print('\hline')
-	print(str(y) + '\\\\')
-	for e in [j for j in journals if int(j['year']) == y]:
-		print(emit_journal(e) + '\\\\')
-
-	for e in [p for p in procs if int(p['year']) == y]:
-		print(emit_proc(e) + '\\\\')
-
-print("")
-
-
+env = Environment(loader=FileSystemLoader('./', encoding='utf8'))
+tpl = env.get_template('pub-tmpl.tex')
+md = tpl.render({'journals': mark_myself(get_type_of_bib(bib, 'article')),
+                 'procs': mark_myself(get_type_of_bib(bib, 'inproceedings')),
+                 'years': ['2017', '2016', '2015', '2014']})
+print(md)
